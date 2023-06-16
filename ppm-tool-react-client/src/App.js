e93e7f6bc6b7aa67bfad983a59014576a754a373
Andrea Simicza <simicza.andrea@gmail.com>
2023-06-16 14:26:42 +0200

    Project structure and routes implemented

--- ppm-tool-react-client/src/App.js
+++ ppm-tool-react-client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Header/>
+          <Routes>
+            <Route exact path="dashboard" Component={Dashboard}/>
+            <Route exact path="addProject" Component={AddProject}/>
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
