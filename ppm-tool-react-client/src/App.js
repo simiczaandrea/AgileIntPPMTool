651db22f1a0306066f8d632e707021e598c9b250
Andrea Simicza <simicza.andrea@gmail.com>
2023-06-30 15:04:16 +0200

    Redux added to react app

--- ppm-tool-react-client/src/App.js
+++ ppm-tool-react-client/src/App.js
@@ -4,18 +4,22 @@ import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-          <Header/>
-          <Routes>
-            <Route exact path="dashboard" Component={Dashboard}/>
-            <Route exact path="addProject" Component={AddProject}/>
-          </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+            <Header/>
+            <Routes>
+              <Route exact path="dashboard" Component={Dashboard}/>
+              <Route exact path="addProject" Component={AddProject}/>
+            </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
